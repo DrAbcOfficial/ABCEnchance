b6d8c0aa430f7a671dab8f873f9abc85c38c72a0
Dr.Abc <dr.abc@cykaskin.com>
2021-08-29 21:26:06 +0800

    viewlag

--- viewmodellag.cpp
+++ viewmodellag.cpp
@@ -1,7 +1,9 @@
 #include <metahook.h>
+#include "math.h"
 #include "mathlib.h"
 #include "local.h"
 #include "viewmodellag.h"
+//copied and edited from here: https://github.com/SNMetamorph/PrimeXT/blob/master/client/r_view.cpp#L266
 void V_CalcViewModelLag(ref_params_t* pparams)
 {
 	static vec3_t m_vecLastFacing;
@@ -13,12 +15,9 @@ void V_CalcViewModelLag(ref_params_t* pparams)
 	vec3_t vOriginalAngles;
 	VectorCopy(*origin,vOriginalOrigin);
 	VectorCopy(*angles, vOriginalAngles);
-
 	// Calculate our drift
 	vec3_t forward, right, up;
-
 	AngleVectors(*angles, forward, right, up);
-
 	if (pparams->frametime != 0.0f)	// not in paused
 	{
 		vec3_t vDifference;
@@ -27,29 +26,24 @@ void V_CalcViewModelLag(ref_params_t* pparams)
 		vDifference[2] = forward[2] - m_vecLastFacing[2];
 
 		float flSpeed = 5.0f;
-
+		// If we start to lag too far behind, we'll increase the "catch up" speed.
+		// Solves the problem with fast cl_yawspeed, m_yaw or joysticks rotating quickly.
+		// The old code would slam lastfacing with origin causing the viewmodel to pop to a new position
 		float flDiff = VectorLength(vDifference);
 		if ((flDiff > gCVars.pModelLagValue->value) && (gCVars.pModelLagValue->value > 0.0f))
-		{
-			float flScale = flDiff / gCVars.pModelLagValue->value;
-			flSpeed *= flScale;
-		}
+			flSpeed *= (flDiff / gCVars.pModelLagValue->value);
 		m_vecLastFacing[0] = m_vecLastFacing[0] + vDifference[0] * (flSpeed * pparams->frametime);
 		m_vecLastFacing[1] = m_vecLastFacing[1] + vDifference[1] * (flSpeed * pparams->frametime);
 		m_vecLastFacing[2] = m_vecLastFacing[2] + vDifference[2] * (flSpeed * pparams->frametime);
 		// Make sure it doesn't grow out of control!!!
 		VectorNormalize(m_vecLastFacing);
-
 		*origin[0] += (vDifference[0] * -1.0f) * flSpeed;
 		*origin[1] += (vDifference[1] * -1.0f) * flSpeed;
 		*origin[2] += (vDifference[2] * -1.0f) * flSpeed;
 	}
 	AngleVectors(pparams->cl_viewangles, forward, right, up);
 	float pitch = pparams->cl_viewangles[PITCH];
-	if (pitch > 180.0f)
-		pitch -= 360.0f;
-	else if (pitch < -180.0f)
-		pitch += 360.0f;
+	pitch += pitch > 180.0f ? -360.0f : pitch < -180.0f ? 360.0f : 0;
 	if (gCVars.pModelLagValue->value <= 0.0f)
 	{
 		VectorCopy(*origin, vOriginalOrigin);
