763b3994d102edfa25821bbc1d021db17979c90c
Dr.Abc <dr.abc@cykaskin.com>
2021-08-31 22:21:09 +0800

    没有烫烫烫

--- deathmsg.h
+++ deathmsg.h
@@ -1,9 +1,15 @@
 #pragma once
 #define MAX_KEEP_DEATHMSG 5
+#define MSG_BUF_SIZE 64
+#define HUD_PRINTNOTIFY 1
+#define MSG_SUICIDENOTIFY L" committed suicide."
+#define MSG_KILLEDNOTIFY L" was killed by a "
+#define MSG_PLAYERKILLNOTIFY L" : (.*) : "
+
 typedef struct {
-	wchar_t* victim;
-	wchar_t* executioner;
-	wchar_t* killer;
+	wchar_t victim[MSG_BUF_SIZE];
+	wchar_t executioner[MSG_BUF_SIZE];
+	wchar_t killer[MSG_BUF_SIZE];
 } deathmsgItem;
 class CHudDeathMsg : public CHudBase
 {
@@ -13,7 +19,7 @@ public:
 	void Reset(void);
 	void InsertNewMsg(const wchar_t* v, const wchar_t* e, const wchar_t* k);
 private:
-	deathmsgItem* aryKeepMsg[MAX_KEEP_DEATHMSG];
+	deathmsgItem aryKeepMsg[MAX_KEEP_DEATHMSG];
 	vgui::HFont HUDFont;
 };
 extern CHudDeathMsg m_HudDeathMsg;
