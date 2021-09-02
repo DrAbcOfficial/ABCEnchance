41e870ae6e10f5a268529f48acd3b3c23cb99903
Dr.Abc <dr.abc@cykaskin.com>
2021-09-02 22:42:51 +0800

    baseweapon_s

--- enginedef.h
+++ enginedef.h
@@ -22,3 +22,23 @@ typedef struct overviewInfo_s
 	float zoom;
 	qboolean rotated;
 }overviewInfo_t;
+
+typedef struct baseweapon_s
+{
+	char szName[128]; //0x0000 
+	int iAmmoType; //0x0080 
+	int iAmmoType2; //0x0084 
+	int iMax; //0x0088 
+	int iMax2; //0x008C 
+	int iSlot; //0x0090 
+	int iPos; //0x0094 
+	int iFlag; //0x0098 
+	int iId; //0x009C 
+	int iClip; //0x00A0 
+	int iClip2; //0x00A4 
+	int bSeleted; //0x00A8 
+	int iUnkown1; //0x00AC 
+	char szName2[128]; //0x00B0 
+	//Idk whats dis, idc whats dis
+	char pad_0x0130[0x588]; //0x0130
+}baseweapon_t;
