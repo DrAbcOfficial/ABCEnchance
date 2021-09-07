262cac89b83a459fd9c66c22fd36f639af67e523
Dr.Abc <dr.abc@cykaskin.com>
2021-09-07 22:02:18 +0800

    roll back

--- drawElement.cpp
+++ drawElement.cpp
@@ -25,6 +25,51 @@ void DrawSPRIcon(int SprHandle, float x, float y, float w, float h, int r, int g
 	gEngfuncs.pTriAPI->End();
 }
 
+void DrawSPRIconClip(int SprHandle, float x, float y, float w, float h, wrect_t* prc, float width, float height, int r, int g, int b, int a)
+{
+	float s1, s2, t1, t2;
+	if (prc)
+	{
+		wrect_t	rc;
+
+		rc = *prc;
+
+		// Sigh! some stupid modmakers set wrong rectangles in hud.txt 
+		if (rc.left <= 0 || rc.left >= width) rc.left = 0;
+		if (rc.top <= 0 || rc.top >= height) rc.top = 0;
+		if (rc.right <= 0 || rc.right > width) rc.right = width;
+		if (rc.bottom <= 0 || rc.bottom > height) rc.bottom = height;
+
+		// calc user-defined rectangle
+		s1 = (float)rc.left / width;
+		t1 = (float)rc.top / height;
+		s2 = (float)rc.right / width;
+		t2 = (float)rc.bottom / height;
+		width = rc.right - rc.left;
+		height = rc.bottom - rc.top;
+	}
+	else
+	{
+		s1 = t1 = 0.0f;
+		s2 = t2 = 1.0f;
+	}
+	gEngfuncs.pTriAPI->SpriteTexture((struct model_s*)gEngfuncs.GetSpritePointer(SprHandle, SprHandle), 0);
+	gEngfuncs.pTriAPI->RenderMode(kRenderTransAdd);
+	gEngfuncs.pTriAPI->CullFace(TRI_NONE);
+	gEngfuncs.pTriAPI->Color4ub(r, g, b, a);
+	gEngfuncs.pTriAPI->Brightness(1);
+	gEngfuncs.pTriAPI->Begin(TRI_QUADS);
+	gEngfuncs.pTriAPI->TexCoord2f(s1, t1);
+	gEngfuncs.pTriAPI->Vertex3f(x, y, 0);
+	gEngfuncs.pTriAPI->TexCoord2f(s2, t1);
+	gEngfuncs.pTriAPI->Vertex3f(x + w, y, 0);
+	gEngfuncs.pTriAPI->TexCoord2f(s2, s2);
+	gEngfuncs.pTriAPI->Vertex3f(x + w, y + h, 0);
+	gEngfuncs.pTriAPI->TexCoord2f(s1, t2);
+	gEngfuncs.pTriAPI->Vertex3f(x, y + h, 0);
+
+	gEngfuncs.pTriAPI->End();
+}
 void DrawSPRIconPos(int SprHandle, vec2_t p1, vec2_t p2, vec2_t p3, vec2_t p4, int r, int g, int b, int a)
 {
 	gEngfuncs.pTriAPI->SpriteTexture((struct model_s*)gEngfuncs.GetSpritePointer(SprHandle, SprHandle), 0);
@@ -55,19 +100,24 @@ int GetHudFontHeight(vgui::HFont m_hFont)
 }
 void GetStringSize(const wchar_t* string, int* width, int* height, vgui::HFont m_hFont)
 {
+	int i;
+	int len;
+	int a, b, c;
+
 	if (width)
 		*width = 0;
+
 	if (height)
 		*height = 0;
+
 	if (!m_hFont)
 		return;
 
-	int len = wcslen(string);
+	len = wcslen(string);
 
 	if (width)
 	{
-		int a, b, c;
-		for (int i = 0; i < len; i++)
+		for (i = 0; i < len; i++)
 		{
 			g_pSurface->GetCharABCwide(m_hFont, string[i], a, b, c);
 			*width += a + b + c;
@@ -75,7 +125,9 @@ void GetStringSize(const wchar_t* string, int* width, int* height, vgui::HFont m
 	}
 
 	if (height)
+	{
 		*height = GetHudFontHeight(m_hFont);
+	}
 }
 int DrawVGUI2String(wchar_t* msg, int x, int y, float r, float g, float b, vgui::HFont m_hFont, bool add = false)
 {
@@ -85,12 +137,15 @@ int DrawVGUI2String(wchar_t* msg, int x, int y, float r, float g, float b, vgui:
 		g /= 255;
 	if (b > 1.0)
 		b /= 255;
+	int i;
 	int iOriginalX;
 	int iTotalLines;
 	int iCurrentLine;
 	int w1, w2, w3;
 	bool bHorzCenter;
+	int len;
 	wchar_t* strTemp;
+	int fontheight;
 
 	if (!m_hFont)
 		return 0;
@@ -99,6 +154,7 @@ int DrawVGUI2String(wchar_t* msg, int x, int y, float r, float g, float b, vgui:
 	iOriginalX = x;
 	iTotalLines = 1;
 	bHorzCenter = false;
+	len = wcslen(msg);
 
 	for (strTemp = msg; *strTemp; strTemp++)
 	{
@@ -107,18 +163,24 @@ int DrawVGUI2String(wchar_t* msg, int x, int y, float r, float g, float b, vgui:
 	}
 
 	if (x == -1)
+	{
 		bHorzCenter = true;
+	}
 
 	if (y == -1)
-		y = (gScreenInfo.iHeight - g_pSurface->GetFontTall(m_hFont)) / 2;
+	{
+		fontheight = g_pSurface->GetFontTall(m_hFont);
+		y = (gScreenInfo.iHeight - fontheight) / 2;
+	}
 
-	for (int i = 0; i < iTotalLines; i++)
+	for (i = 0; i < iTotalLines; i++)
 	{
 		wchar_t line[1024];
 		int iWidth;
 		int iHeight;
 		int iTempCount;
 		int j;
+		int shadow_x;
 
 		iTempCount = 0;
 		iWidth = 0;
@@ -145,7 +207,7 @@ int DrawVGUI2String(wchar_t* msg, int x, int y, float r, float g, float b, vgui:
 		if (!add)
 		{
 			gEngfuncs.pfnDrawSetTextColor(0, 0, 0);
-			int shadow_x = x;
+			shadow_x = x;
 			for (j = 0; j < iTempCount; j++)
 			{
 				gEngfuncs.pfnVGUI2DrawCharacter(shadow_x, y, line[j], m_hFont);
