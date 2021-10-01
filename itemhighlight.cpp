9f33b3c57327c82fa94c1b1a952dcbdce61ea1e6
Dr.Abc <dr.abc@cykaskin.com>
2021-10-02 01:07:04 +0800

    itemhighlight

--- itemhighlight.cpp
+++ itemhighlight.cpp
@@ -1,11 +1,13 @@
 #include <metahook.h>
 #include <map>
+#include "pm_defs.h"
+#include "com_model.h"
 #include "cl_entity.h"
 #include "event_api.h"
-#include "com_model.h"
 #include "triangleapi.h"
 #include "mathlib.h"
 #include "cvardef.h"
+#include "extraprecache.h"
 
 #include "vguilocal.h"
 #include "local.h"
@@ -84,7 +86,6 @@ int CHudItemHighLight::Init(){
 	gCVars.pItemHighLightRange = CREATE_CVAR("cl_itemhighlightrange", "344", FCVAR_VALUE, RangeSizeCallBack);
 	return 0;
 }
-
 void CHudItemHighLight::Reset(){
 	m_mapHighLightTable.clear();
 	m_mapToBeDraw.clear();
@@ -93,8 +94,48 @@ void CHudItemHighLight::Reset(){
 		if (aryHighLightList[i].Index > 0)
 			m_mapHighLightTable[aryHighLightList[i].Index] = &aryHighLightList[i];
 	}
+	m_iHighLightMdl = PrecacheExtraModel("abcenchance/mdl/item_highlight.mdl");
 }
-
+void HighLightTentCallBack(TEMPENTITY* ent, float frametime, float currenttime) {
+	cl_entity_t* var = gEngfuncs.GetEntityByIndex(ent->clientIndex);
+	if (!var) {
+		ent->die = 0;
+		return;
+	}
+	VectorCopy(var->angles, ent->entity.angles);
+	VectorCopy(var->curstate.origin, ent->entity.origin);
+	VectorAdd(ent->entity.origin, ent->tentOffset, ent->entity.origin);
+	ent->entity.angles[1] += ent->bounceFactor;
+}
+void CHudItemHighLight::CreateHighLight(cl_entity_t* var) {
+	if (m_mapRestoredTent.count(var))
+		return;
+	model_t* mdl = gEngfuncs.hudGetModelByIndex(m_iHighLightMdl);
+	if (!mdl)
+		return;
+	TEMPENTITY* ent1 = gHookFuncs.CL_TempEntAllocHigh(var->curstate.origin, mdl);
+	TEMPENTITY* ent2 = gHookFuncs.CL_TempEntAllocHigh(var->curstate.origin, mdl);
+	if ((!ent1) || (!ent2))
+		return;
+	ent1->flags = ent2->flags = FTENT_FADEOUT | FTENT_CLIENTCUSTOM;
+	ent1->clientIndex = ent2->clientIndex = var->index;
+	ent1->die = ent2->die = gEngfuncs.GetClientTime() + 999.0f;
+	VectorCopy(var->curstate.maxs, ent1->tentOffset);
+	VectorCopy(var->curstate.mins, ent2->tentOffset);
+	/*角度不对*/
+	ent1->bounceFactor = -90;
+	ent2->bounceFactor = 90;
+	ent1->entity.curstate.skin = ent2->entity.curstate.skin = m_mapHighLightTable[var->curstate.modelindex]->Type;
+	ent1->callback = ent2->callback = HighLightTentCallBack;
+	m_mapRestoredTent[var].first = ent1;
+	m_mapRestoredTent[var].second = ent2;
+}
+void CHudItemHighLight::EraseHighLight(cl_entity_t* var) {
+	if (!m_mapRestoredTent.count(var))
+		return;
+	m_mapRestoredTent[var].first->die = m_mapRestoredTent[var].second->die = 0;
+	m_mapRestoredTent.erase(var);
+}
 void CHudItemHighLight::Draw(float flTime){
 	if (gCVars.pItemHighLight->value <= 0)
 		return;
@@ -103,6 +144,7 @@ void CHudItemHighLight::Draw(float flTime){
 	for (it = m_mapToBeDraw.begin(); it != m_mapToBeDraw.end(); ){
 		cl_entity_t* var = (*it).second;
 		if (!var) {
+			EraseHighLight(var);
 			it = m_mapToBeDraw.erase(it);
 			continue;
 		}
@@ -110,22 +152,11 @@ void CHudItemHighLight::Draw(float flTime){
 		VectorSubtract(var->curstate.origin, local->curstate.origin, len);
 		if (var->curstate.messagenum != local->curstate.messagenum || 
 			VectorLength(len) > gCVars.pItemHighLightRange->value) {
+			EraseHighLight(var);
 			it = m_mapToBeDraw.erase(it);
 		}
 		else {
-			//TODO: 换成高亮描边
-			vec3_t vecHUD;
-			gEngfuncs.pTriAPI->WorldToScreen(var->curstate.origin, vecHUD);
-			vecHUD[0] = (1.0f + vecHUD[0]) * gScreenInfo.iWidth / 2;
-			vecHUD[1] = (1.0f - vecHUD[1]) * gScreenInfo.iHeight / 2;
-			int r = 0, g = 0, b = 0;
-			switch (m_mapHighLightTable[var->curstate.modelindex]->Type)
-			{
-			case 0:r = 255; break;
-			case 1:g = 255; break;
-			case 2:b = 255; break;
-			}
-			gEngfuncs.pfnFillRGBABlend(vecHUD[0], vecHUD[1], 8, 8,  r, g, b, 80);
+			CreateHighLight(var);
 			it++;
 		}
 	}
