5b9e486fa03aae94ec88b9ea0eb519a5f9077184
Dr.Abc <dr.abc@cykaskin.com>
2021-10-02 23:56:50 +0800

    deathmsg

--- deathmsg.cpp
+++ deathmsg.cpp
@@ -168,7 +168,8 @@ void CHudDeathMsg::InsertNewDeathMsg(wchar_t* v, wchar_t* e, wchar_t* k, int kIn
 	aryKeepMsg[0].isYou = bIsYou;
 	aryKeepMsg[0].damageType = iDamageType;
 	wcscpy_s(aryKeepMsg[0].killer, L"");
-	if (kIndex == 0) {
+	cl_entity_t* killer = gEngfuncs.GetEntityByIndex(kIndex);
+	if (!killer || killer->model->type == mod_brush) {
 		switch (iDamageType) {
 		case 1: case 2: case 4: case 8: case 16:
 		case 32: case 64: case 128: case 256: case 512:
