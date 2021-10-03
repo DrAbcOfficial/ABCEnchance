cac8406ca8ce311e886a19f3a2f1fd9a6b873103
Dr.Abc <dr.abc@cykaskin.com>
2021-10-03 23:28:41 +0800

    添加最小高亮检查

--- exportfuncs.h
+++ exportfuncs.h
@@ -4,7 +4,7 @@
 #include "enginedef.h"
 #include "r_studioint.h"
 
-#define PLUGIN_VERSION 100000
+#define PLUGIN_VERSION 100001
 
 extern cl_enginefunc_t gEngfuncs;
 extern cl_exportfuncs_t gExportfuncs;
