de03de07c5482a6ada4cbd41f3d88b1a1ec3083b
Dr.Abc <dr.abc@cykaskin.com>
2022-02-11 15:53:51 +0800

    llvm

--- gl_shader.h
+++ gl_shader.h
@@ -38,5 +38,5 @@ void GL_MultiTexCoord3f(GLenum target, float s, float t, float r);
 void GL_FreeShaders();
 
 #define SHADER_DEFINE(name) name##_program_t name;
-#define SHADER_UNIFORM(name, loc, locstring) name##.loc = glGetUniformLocationARB(name.program, locstring);
-#define SHADER_ATTRIB(name, loc, locstring) name##.loc = glGetAttribLocationARB(name.program, locstring);
+#define SHADER_UNIFORM(name, loc, locstring) name.loc = glGetUniformLocationARB(name.program, locstring);
+#define SHADER_ATTRIB(name, loc, locstring) name.loc = glGetAttribLocationARB(name.program, locstring);
