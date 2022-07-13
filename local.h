fd075a1ba2c7055dba3206a41e4a8ad40191b458
Abc Dr <641503229@qq.com>
2022-07-13 16:18:53 +0800

    steamid.cpp

--- local.h
+++ local.h
@@ -3,8 +3,12 @@
 #include "com_model.h"
 
 #define FCVAR_VALUE (FCVAR_PRINTABLEONLY | FCVAR_CLIENTDLL | FCVAR_ARCHIVE)
+
 #define BASE_GWR_SELECTED 842100225
 #define BASE_GWR_UNSELECTED 842100224
+
+#define V_snprintf sprintf_s
+
 #define HOOK_COMMAND(x, y) g_pMetaHookAPI->HookCmd((char*)x, __UserCmd_##y)
 #define ADD_COMMAND(x, y) gEngfuncs.pfnAddCommand((char*)x, y)
 #define ConsoleWriteline(x) gEngfuncs.Con_Printf(x)
