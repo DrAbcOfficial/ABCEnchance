f291a247d24bc7cb46be9626f9b7de53ef7f8749
Dr.Abc <dr.abc@cykaskin.com>
2023-06-28 23:18:34 +0800

    too stupid

--- src/Source/exportfuncs.cpp
+++ src/Source/exportfuncs.cpp
@@ -645,9 +645,7 @@ int HUD_VidInit(void){
 	//Search and destory vanillia HUDs
 	if (g_dwHUDListAddr && gCVars.pDynamicHUD->value > 0) {
 		HUDLIST* pHudList = (HUDLIST*)(*(DWORD*)(g_dwHUDListAddr + 0x0));
-#if _DEBUG
 		size_t iter = 0;
-#endif // _DEBUG
 		while (pHudList) {
 			switch (iter) {
 			case 0: gHookHud.m_Health = (CHudHealth*)pHudList->p; break;
@@ -656,12 +654,10 @@ int HUD_VidInit(void){
 			case 4: gHookHud.m_Flash = (CHudFlashlight*)pHudList->p; break;
 			}
 			pHudList = pHudList->pNext;
-#if _DEBUG
 			// Use RTTI to know which HUD
 			//if (dynamic_cast<CHudBattery*>(pHudList->p) != nullptr)
 			//	gHookHud.m_Battery = (dynamic_cast<CHudBattery*>(pHudList->p));
 			iter++;
-#endif // _DEBUG
 		}
 	}
 	if (g_pViewPort)
