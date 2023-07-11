e66ae418f497cdd26822919f78abf214f5b87443
Dr.Abc <dr.abc@cykaskin.com>
2023-07-11 20:01:24 +0800

    fit scale

--- src/Source/HUD/vgui/textmenu.cpp
+++ src/Source/HUD/vgui/textmenu.cpp
@@ -68,8 +68,9 @@ void CTextMenu::SetContent(const char* szMenu){
 	m_pMenu->SetText(szMenu);
 	int w, h;
 	m_pMenu->GetTextImage()->GetContentSize(w, h);
-	SetWide(w + 4);
-	SetTall(h + 4);
+	SetWide(w * 1.1f);
+	SetTall(h * 1.1f);
+	m_pMenu->SetPos(GetWide() * 0.05, GetTall() * 0.05);
 }
 
 
