84d200cd0dfac6aa019b5a234188dcaa59372093
Dr.Abc <dr.abc@cykaskin.com>
2023-08-06 19:02:27 +0800

    move vgui::surface()->CreateNewTextureID(true) to ctor #28

--- VGUI2/BasePanel.h
+++ VGUI2/BasePanel.h
@@ -1,3 +1,12 @@
+enum EBackgroundState{
+	BACKGROUND_INITIAL,
+	BACKGROUND_LOADING,
+	BACKGROUND_MAINMENU,
+	BACKGROUND_LEVEL,
+	BACKGROUND_DISCONNECTED,
+	BACKGROUND_EXITING,
+};
+
 void BasePanel_InstallHook(void);
 void BackGroundVideoInit();
 void BackGroundVideoClose();
