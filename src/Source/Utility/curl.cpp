fa073247bf79ff2ec1335a346684cb6e7aadafd8
Dr.Abc <me@drabc.net>
2023-08-27 15:14:15 +0800

    netease api

--- src/Source/Utility/curl.cpp
+++ src/Source/Utility/curl.cpp
@@ -3,6 +3,7 @@
 
 void* (*curl_easy_init)(void) = nullptr;
 CURLcode(*curl_easy_setopt)(void* curl, CURLoption option, ...) = nullptr;
+CURLcode (*curl_easy_getinfo)(void* curl, CURLINFO info, ...) = nullptr;
 CURLcode(*curl_easy_perform)(void* curl) = nullptr;
 void (*curl_easy_cleanup)(void* curl) = nullptr;
 struct curl_slist* (*curl_slist_append)(struct curl_slist*, const char*) = nullptr;
@@ -17,6 +18,7 @@ void LoadLibcurl() {
 	LIBCURL_FUNCTION_DEFINE(curl_easy_init);
 	LIBCURL_FUNCTION_DEFINE(curl_slist_append);
 	LIBCURL_FUNCTION_DEFINE(curl_easy_setopt);
+	LIBCURL_FUNCTION_DEFINE(curl_easy_getinfo);
 	LIBCURL_FUNCTION_DEFINE(curl_easy_perform);
 	LIBCURL_FUNCTION_DEFINE(curl_slist_free_all);
 	LIBCURL_FUNCTION_DEFINE(curl_easy_cleanup);
