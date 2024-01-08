aa900678d8a3c93941d47e706986e0866291dca8
Abc Dr <me@drabc.net>
2024-01-09 01:22:21 +0800

    better Task Manager

--- src/Source/Utility/Task.cpp
+++ src/Source/Utility/Task.cpp
@@ -5,69 +5,22 @@ static CTaskManager s_TaskManager;
 CTaskManager* GetTaskManager() {
 	return &s_TaskManager;
 }
-
-CTaskItem::CTaskItem(std::future<std::any>& futrue){
-	auto& refB = m_bReady;
-	auto& refV = m_pValue;
-	m_pThread = std::thread(
-		[&refB, &refV, futrue = std::move(futrue)]() mutable{
-			refV = futrue.get();
-			refB = true;
-		}
-	);
-}
-
-CTaskItem::~CTaskItem(){
-	if (m_pThread.joinable())
-		m_pThread.join();
-}
-
-CTaskItem* CTaskItem::ContinueWith(std::function<void(std::any&)> func){
-	m_pContinue = std::move(func);
-	return this;
-}
-
-bool CTaskItem::IsReady(){
-	return m_bReady;
-}
-
-std::function<void(std::any&)> CTaskItem::GetContinue(){
-	return m_pContinue;
-}
-
-std::any CTaskItem::GetValue(){
-	return m_pValue;
-}
-
-CTaskItem* CTaskItem::Start(){
-	m_pThread.detach();
-	return this;
-}
-
-CTaskItem* CTaskManager::Add(std::future<std::any>& func){
-	CTaskItem* item = new CTaskItem(func);
-	m_aryPending.push_back(item);
-	return item;
-}
-
-bool CTaskManager::Has(CTaskItem* check){
+bool CTaskManager::Has(ITaskItem* check){
 	for (auto iter = m_aryList.begin(); iter != m_aryList.end(); iter++) {
 		if ((*iter) == check)
 			return true;
 	}
 	return false;
 }
-
 void CTaskManager::CheckAll(){
 	for (auto iter = m_aryList.begin(); iter != m_aryList.end();) {
 		auto item = *iter;
 		if (item->IsReady()) {
 			//转移所有权，防止栈被干烂
-			auto callback = std::move(item->GetContinue());
-			auto val = std::move(item->GetValue());
+			auto caller = std::move(item->GetCaller());
 			delete item;
 			iter = m_aryList.erase(iter);
-			std::invoke(callback, val);
+			std::invoke(caller);
 		}
 		else
 			iter++;
