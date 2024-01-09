6f364a8930c95b34a33486f3347101b6e53f0b57
Dr.Abc <me@drabc.net>
2024-01-09 16:07:44 +0800

    vgui  radar

--- src/Header/HUD/radar.h
+++ src/Header/HUD/radar.h
@@ -5,12 +5,17 @@
 #include <vgui_controls/EditablePanel.h>
 #include "IViewportPanel.h"
 
+namespace vgui {
+	class ImagePanel;
+}
+
 class CRadarPanel : public vgui::EditablePanel, public IViewportPanel {
 public:
 	DECLARE_CLASS_SIMPLE(CRadarPanel, vgui::EditablePanel);
 	
 	CRadarPanel();
 	~CRadarPanel();
+	virtual void PerformLayout() override;
 	virtual void Paint() override;
 	virtual void OnThink() override;
 	virtual void ApplySettings(KeyValues* inResourceData) override;
@@ -23,36 +28,27 @@ public:
 	virtual vgui::VPANEL GetVPanel() override;
 	virtual void SetParent(vgui::VPANEL parent) override;
 
-	void SetScaleTime(float time);
+	void SetScale(bool state);
 	void BlitFramebuffer();
-
-	float flFinishScaleTime;
 private:
-	int m_iBackground;
-	int m_iRoundBackground;
-	int m_iUpGround;
-	int m_iNorth;
-	int m_iViewAngle;
+	vgui::ImagePanel* m_pBackground;
+	vgui::ImagePanel* m_pRoundBackground;
+	vgui::ImagePanel* m_pMapground;
+	vgui::ImagePanel* m_pUpground;
+	vgui::ImagePanel* m_pNorthground;
+	vgui::ImagePanel* m_pViewangleground;
 
-	float m_flRadarSclaeTime;
 	float m_flRoundRadius;
-	int m_iGapSize;
-	int m_iAngleSize;
-	int m_iNorthSize;
-
-	char m_szBackgroundImg[MAX_PATH];
-	char m_szBackgroundRoundImg[MAX_PATH];
-	char m_szUpgroundImg[MAX_PATH];
-	char m_szNorthImg[MAX_PATH];
-	char m_szViewangleImg[MAX_PATH];
-	
 	Color m_cOutline;
 	Color m_cMap;
 
 	uint m_hRadarBufferFBO;
-	uint m_hRadarBufferTex;
+
+	int m_iStartWidth;
+	int m_iStartTall;
 
 	float flNextUpdateTrTime;
+	bool m_bInScale;
 
 	size_t iOverviewR = 0;
 	size_t iOverviewG = 0;
