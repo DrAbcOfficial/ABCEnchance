062389cc0db21f3ada6b3e2ed04595c8e3498774
Dr.Abc <me@drabc.net>
2024-01-13 18:18:50 +0800

    signal

--- VGUI2/GameUI/GameConsole.cpp
+++ VGUI2/GameUI/GameConsole.cpp
@@ -18,59 +18,47 @@ void(__fastcall* m_pfnCGameConsole_SetParent)(void* pthis, int, int parent);
 
 class CGameConsole : public IGameConsole{
 public:
-	virtual void Activate(void);
-	virtual void Initialize(void);
-	virtual void Hide(void);
-	virtual void Clear(void);
-	virtual bool IsConsoleVisible(void);
-	virtual void Printf(const char* format, ...);
-	virtual void DPrintf(const char* format, ...);
+	virtual void Activate(void) {
+		g_ConsoleDialog->Activate();
+	}
+	virtual void Initialize(void) {
+		g_ConsoleDialog = new vgui::CConsoleDialog(nullptr, "Console", false);
+		g_ConsoleDialog->ColorPrint(Color(0, 255, 0, 200), "[ABCEnchance] ABCEnchance has run successfully!\n");
+	}
+	virtual void Hide(void) {
+		g_ConsoleDialog->Hide();
+	}
+	virtual void Clear(void) {
+		g_ConsoleDialog->Clear();
+	}
+	virtual bool IsConsoleVisible(void) {
+		return g_ConsoleDialog->IsVisible();
+	}
+	virtual void Printf(const char* format, ...) {
+		va_list args;
+		va_start(args, format);
+		static char buf[1024];
+		Q_vsnprintf(buf, sizeof(buf), format, args);
+		g_ConsoleDialog->Print(buf);
+		va_end(args);
+	}
+	virtual void DPrintf(const char* format, ...) {
+		va_list args;
+		va_start(args, format);
+		static char buf[1024];
+		Q_vsnprintf(buf, sizeof(buf), format, args);
+		g_ConsoleDialog->DPrint(buf);
+		va_end(args);
+	}
 	//virtual void ColorPrintf(Color &clr) = 0;	// Doesn't exist on GS
-	virtual void SetParent(int parent);
+	virtual void SetParent(int parent) {
+		g_ConsoleDialog->SetParent(parent);
+	}
 };
 
-void CGameConsole::Activate(void){
-	g_ConsoleDialog->Activate();
-}
-void CGameConsole::Initialize(void){
-	g_ConsoleDialog = new vgui::CConsoleDialog(nullptr, "Console", false);
-	g_ConsoleDialog->ColorPrint(Color(0, 255, 0, 200), "[ABCEnchance] ABCEnchance has run successfully!\n");
-}
-void CGameConsole::Hide(void){
-	g_ConsoleDialog->Hide();
-}
-void CGameConsole::Clear(void){
-	g_ConsoleDialog->Clear();
-}
-bool CGameConsole::IsConsoleVisible(void){
-	return g_ConsoleDialog->IsVisible();
-}
-void CGameConsole::Printf(const char* format, ...){
-	va_list args;
-	va_start(args, format);
-	static char buf[1024];
-	Q_vsnprintf(buf, sizeof(buf), format, args);
-	g_ConsoleDialog->Print(buf);
-	va_end(args);
-}
-void CGameConsole::DPrintf(const char* format, ...){
-	va_list args;
-	va_start(args, format);
-	static char buf[1024];
-	Q_vsnprintf(buf, sizeof(buf), format, args);
-	g_ConsoleDialog->DPrint(buf);
-	va_end(args);
-}
-void CGameConsole::SetParent(int parent){
-	g_ConsoleDialog->SetParent(parent);
-}
-
 static CGameConsole s_GameConsole;
-
 IGameConsole* gameconsolefuncs;
-
-void GameConsole_InstallHook(void)
-{
+void GameConsole_InstallHook(void){
 	HINTERFACEMODULE hGameUI = (HINTERFACEMODULE)GetModuleHandle("GameUI.dll");
 	if (hGameUI) {
 		CreateInterfaceFn fnCreateInterface = Sys_GetFactory(hGameUI);
