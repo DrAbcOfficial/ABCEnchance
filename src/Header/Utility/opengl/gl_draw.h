37eb22e5a8ef9d1082f5b3043e718daaae1104bc
Dr.Abc <me@drabc.net>
2024-01-13 21:37:23 +0800

    1

--- src/Header/Utility/opengl/gl_draw.h
+++ src/Header/Utility/opengl/gl_draw.h
@@ -1,7 +1,7 @@
 #pragma once
 void glBind(GLint tex);
 void DrawSPRIcon(int SprHandle, int mode, float x, float y, float w, float h, int r, int g, int b, int a, int frame = 0);
-void DrawSPRIconPos(int SprHandle, int mode, vec2_t p1, vec2_t p2, vec2_t p3, vec2_t p4, int r, int g, int b, int a);
+void DrawSPRIconPos(int SprHandle, int mode, float p1[2], float p2[2], float p3[2], float p4[2], int r, int g, int b, int a);
 int GetHudFontHeight(vgui::HFont m_hFont);
 void GetStringSize(const wchar_t* string, int* width, int* height, vgui::HFont m_hFont);
 int DrawVGUI2String(wchar_t* msg, int x, int y, int r, int g, int b, vgui::HFont m_hFont, bool add = false);
--- vgui_controls/GaussianBlurPanel.cpp
+++ vgui_controls/GaussianBlurPanel.cpp
@@ -9,6 +9,7 @@
 #include <glew.h>
 #include <gl_draw.h>
 #include <gl_utility.h>
+#include <vguilocal.h>
 
 
 using namespace vgui;
@@ -16,6 +17,12 @@ using namespace vgui;
 // Purpose: 
 //-----------------------------------------------------------------------------
 GaussianBlurPanel::GaussianBlurPanel(Panel *parent, const char *name) : Panel(parent, name){
+	glGenFramebuffers(1, &m_hBufferFBO);
+	m_hBufferTex = GL_GenTextureRGBA8(ScreenWidth(), ScreenHeight());
+	glGetIntegerv(GL_FRAMEBUFFER_BINDING, &m_oldFrameBuffer);
+	glBindFramebuffer(GL_FRAMEBUFFER, m_hBufferFBO);
+	glFramebufferTexture(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, m_hBufferTex, 0);
+	glBindFramebuffer(GL_FRAMEBUFFER, m_oldFrameBuffer);
 }
 
 vgui::GaussianBlurPanel::~GaussianBlurPanel(){
@@ -24,18 +31,7 @@ vgui::GaussianBlurPanel::~GaussianBlurPanel(){
 	if (m_hBufferTex)
 		glDeleteTextures(1, &m_hBufferTex);
 }
-void vgui::GaussianBlurPanel::SetupTexture(){
-	if (!m_hBufferFBO && !m_hBufferTex) {
-		glGenFramebuffers(1, &m_hBufferFBO);
-		int w, h;
-		GetSize(w, h);
-		m_hBufferTex = GL_GenTextureRGBA8(w, h);
-		glGetIntegerv(GL_FRAMEBUFFER_BINDING, &m_oldFrameBuffer);
-		glBindFramebuffer(GL_FRAMEBUFFER, m_hBufferFBO);
-		glFramebufferTexture(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, m_hBufferTex, 0);
-		glBindFramebuffer(GL_FRAMEBUFFER, m_oldFrameBuffer);
-	}
-}
+
 
 float vgui::GaussianBlurPanel::GetBlurRatio(){
 	return m_flRatio;
@@ -50,24 +46,38 @@ void vgui::GaussianBlurPanel::SetBlurRatio(float f){
 // Purpose: 
 //-----------------------------------------------------------------------------
 void GaussianBlurPanel::PaintBackground(){
-	int w, h;
-	GetSize(w, h);
-	int x, y;
-	vgui::ipanel()->GetAbsPos(GetVPanel(), x, y);
+	int x, y, w, h;
+	GetBounds(x, y, w, h);
 	glGetIntegerv(GL_FRAMEBUFFER_BINDING, &m_oldFrameBuffer);
+	GL_BlitFrameBufferToFrameBufferColorOnly(m_oldFrameBuffer, m_hBufferFBO, ScreenWidth(), ScreenHeight(), ScreenWidth(), ScreenHeight());
 	glBindFramebuffer(GL_FRAMEBUFFER, m_hBufferFBO);
-	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-	glBindFramebuffer(GL_DRAW_FRAMEBUFFER, m_hBufferFBO);
-	glBindFramebuffer(GL_READ_FRAMEBUFFER, m_oldFrameBuffer);
-	glBlitFramebuffer(x, y, x + w, x + h, 0, 0, w, h, GL_COLOR_BUFFER_BIT, GL_LINEAR);
-	DrawGaussianBlur(m_hBufferTex, m_flRatio, w, h);
+	
+	DrawGaussianBlur(m_hBufferTex, m_flRatio, ScreenWidth(), ScreenHeight());
+
 	glBindFramebuffer(GL_FRAMEBUFFER, m_oldFrameBuffer);
 
 	glEnable(GL_TEXTURE_2D);
 	glBind(m_hBufferTex);
 	Color bgcolor = GetBgColor();
 	glColor4ub(bgcolor.r(), bgcolor.g(), bgcolor.b(), bgcolor.a());
-	DrawQuadPos(0, 0, w, h);
+
+	int absx, absy;
+	vgui::ipanel()->GetAbsPos(GetVPanel(), absx, absy);
+	absy = ScreenHeight() - absy - h;
+	float rx = (float)absx / ScreenWidth();
+	float ry = (float)absy / ScreenHeight();
+	float rw = (float)w / ScreenWidth();
+	float rh = (float)h / ScreenHeight();
+	glBegin(GL_QUADS);
+	glTexCoord2f(rx, ry);
+	glVertex2f(x, y + h);
+	glTexCoord2f(rx, ry + rh);
+	glVertex2f(x, y);
+	glTexCoord2f(rx + rw, ry + rh);
+	glVertex2f(x + w, y);
+	glTexCoord2f(rx + rw, ry);
+	glVertex2f(x + w, y + h);
+	glEnd();
 	glDisable(GL_TEXTURE_2D);
 }
 
