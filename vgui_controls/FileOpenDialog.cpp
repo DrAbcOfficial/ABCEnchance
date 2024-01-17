771bf912458410e39da07c6d8cc4d7b08bb4746d
Dr.Abc <me@drabc.net>
2024-01-17 17:56:41 +0800

    wildcard performance

--- vgui_controls/FileOpenDialog.cpp
+++ vgui_controls/FileOpenDialog.cpp
@@ -211,41 +211,31 @@ std::time_t ToTimeT(TP tp){
 	return system_clock::to_time_t(sctp);
 }
 bool WildMatch(const std::string& str, const std::string& pat) {
-	std::string::const_iterator str_it = str.begin();
-	for (std::string::const_iterator pat_it = pat.begin(); pat_it != pat.end();
-		++pat_it) {
-		switch (*pat_it) {
-		case '?':
-			if (str_it == str.end()) {
-				return false;
-			}
-
-			++str_it;
-			break;
-		case '*': {
-			if (pat_it + 1 == pat.end()) {
-				return true;
-			}
-
-			const size_t max = strlen(&*str_it);
-			for (size_t i = 0; i < max; ++i) {
-				if (WildMatch(&*(pat_it + 1), &*(str_it + i))) {
-					return true;
-				}
-			}
-
+	const char* str_ptr = str.c_str();
+	const char* pat_ptr = pat.c_str();
+	const char* star_ptr = nullptr;
+	const char* match_ptr = nullptr;
+	while (*str_ptr) {
+		if (*pat_ptr == '?' || *str_ptr == *pat_ptr) {
+			str_ptr++;
+			pat_ptr++;
+		}
+		else if (*pat_ptr == '*') {
+			star_ptr = pat_ptr;
+			match_ptr = str_ptr;
+			pat_ptr++;
+		}
+		else if (star_ptr) {
+			pat_ptr = star_ptr + 1;
+			match_ptr++;
+			str_ptr = match_ptr;
+		}
+		else
 			return false;
-		}
-		default:
-			if (*str_it != *pat_it) {
-				return false;
-			}
-
-			++str_it;
-		}
 	}
-
-	return str_it == str.end();
+	while (*pat_ptr == '*')
+		pat_ptr++;
+	return !*pat_ptr;
 }
 
 namespace vgui
