5d276dcb8f49257a4df41fee74fedc6e4b4fee4c
Dr.Abc <me@drabc.net>
2024-01-17 17:28:58 +0800

    win32 helper

---
+++ src/Header/Utility/win32helper.h
@@ -0,0 +1,14 @@
+#pragma once
+#ifdef WIN32
+#include <string>
+#include <vector>
+
+const char* Win32GetAttributesAsString(DWORD dwAttributes);
+std::string Win32GetFileDescriptionByExtension(const char* extension);
+std::string Win32GetStringValueFromRegistry(HKEY hKey, const std::string& subKey, const std::string& valueName);
+std::string Win32GetDefaultValueFromRegistry(HKEY hKey, const std::string& subKey);
+HICON Win32GetFileIconByExtension(const std::string& extension);
+BITMAP Win32GetBitmapInfo(HICON hIcon);
+std::vector<unsigned char> Win32ConvertHICONToRGBA(HICON hIcon, size_t& ww, size_t& hh);
+#endif // WIN32
+
--- vgui_controls/FileOpenDialog.cpp
+++ vgui_controls/FileOpenDialog.cpp
@@ -61,6 +61,7 @@
 using namespace vgui;
 namespace fs = std::filesystem;
 extern bool IsOSX();
+extern std::string Win32GetFileDescriptionByExtension(const char* extension);
 
 static int s_nLastSortColumn = 0;
 
@@ -253,208 +254,9 @@ bool WildMatch(const std::string& str, const std::string& pat) {
 
 	return str_it == str.end();
 }
-class IconImage : public IImage
-{
-public:
-	IconImage(unsigned char* data, size_t w, size_t h) {
-		_id = surface()->CreateNewTextureID();
-		_wide = w;
-		_tall = h;
-		_color = Color(255, 255, 255, 255);
-		surface()->DrawSetTextureRGBA(_id, data, w, h, 1, 0);
-	}
-
-public:
-	virtual void Paint(void) {
-		if (!_id)
-			_id = surface()->CreateNewTextureID();
-		surface()->DrawSetColor(_color[0], _color[1], _color[2], _color[3]);
-		surface()->DrawSetTexture(_id);
-		if (_wide == 0)
-			GetSize(_wide, _tall);
-		surface()->DrawTexturedRect(_pos[0], _pos[1], _pos[0] + _wide, _pos[1] + _tall);
-	}
-	virtual void GetSize(int& wide, int& tall) {
-		wide = 0;
-		tall = 0;
-		if (0 == _wide && 0 == _tall)
-			surface()->DrawGetTextureSize(_id, _wide, _tall);
-		wide = _wide;
-		tall = _tall;
-	}
-	virtual void GetContentSize(int& wide, int& tall) {
-		GetSize(wide, tall);
-	}
-	virtual void SetSize(int x, int y) {
-		_wide = x;
-		_tall = y;
-	}
-	virtual void SetPos(int x, int y) {
-		_pos[0] = x;
-		_pos[1] = y;
-	}
-
-	virtual void SetColor(Color col) {
-		_color = col;
-	}
-private:
-	HTexture _id;
-	int _pos[2];
-	Color _color;
-	int _wide, _tall;
-};
-
-#ifdef WIN32	
-const char* Win32GetAttributesAsString(DWORD dwAttributes)
-{
-	static char out[256];
-	out[0] = 0;
-	if (dwAttributes & FILE_ATTRIBUTE_ARCHIVE)
-	{
-		Q_strncat(out, "A", sizeof(out), COPY_ALL_CHARACTERS);
-	}
-	if (dwAttributes & FILE_ATTRIBUTE_COMPRESSED)
-	{
-		Q_strncat(out, "C", sizeof(out), COPY_ALL_CHARACTERS);
-	}
-	if (dwAttributes & FILE_ATTRIBUTE_DIRECTORY)
-	{
-		Q_strncat(out, "D", sizeof(out), COPY_ALL_CHARACTERS);
-	}
-	if (dwAttributes & FILE_ATTRIBUTE_HIDDEN)
-	{
-		Q_strncat(out, "H", sizeof(out), COPY_ALL_CHARACTERS);
-	}
-	if (dwAttributes & FILE_ATTRIBUTE_READONLY)
-	{
-		Q_strncat(out, "R", sizeof(out), COPY_ALL_CHARACTERS);
-	}
-	if (dwAttributes & FILE_ATTRIBUTE_SYSTEM)
-	{
-		Q_strncat(out, "S", sizeof(out), COPY_ALL_CHARACTERS);
-	}
-	if (dwAttributes & FILE_ATTRIBUTE_TEMPORARY)
-	{
-		Q_strncat(out, "T", sizeof(out), COPY_ALL_CHARACTERS);
-	}
-	return out;
-}
-const char* Win32GetFileTimetamp(FILETIME ft){
-	SYSTEMTIME local;
-	FILETIME localFileTime;
-	FileTimeToLocalFileTime(&ft, &localFileTime);
-	FileTimeToSystemTime(&localFileTime, &local);
-
-	static char out[256];
-
-	bool am = true;
-	WORD hour = local.wHour;
-	if (hour >= 12)
-	{
-		am = false;
-		// 12:42 pm displays as 12:42 pm
-		// 13:42 pm displays as 1:42 pm
-		if (hour > 12)
-		{
-			hour -= 12;
-		}
-	}
-	Q_snprintf(out, sizeof(out), "%d/%02d/%04d %d:%02d %s",
-		local.wMonth,
-		local.wDay,
-		local.wYear,
-		hour,
-		local.wMinute,
-		am ? "AM" : "PM" // TODO: Localize this?
-	);
-	return out;
-}
-std::string Win32GetStringValueFromRegistry(HKEY hKey, const std::string& subKey, const std::string& valueName) {
-	std::string result;
-	DWORD size = 0;
-	if (RegGetValueA(hKey, subKey.c_str(), valueName.c_str(), RRF_RT_REG_SZ, NULL, NULL, &size) == ERROR_SUCCESS) {
-		char* buffer = new char[size];
-		if (RegGetValueA(hKey, subKey.c_str(), valueName.c_str(), RRF_RT_REG_SZ, NULL, buffer, &size) == ERROR_SUCCESS)
-			result = std::string(buffer, size - 1);
-		delete[] buffer;
-	}
-	return result;
-}
-std::string Win32GetDefaultValueFromRegistry(HKEY hKey, const std::string& subKey) {
-	return Win32GetStringValueFromRegistry(hKey, subKey, "");
-}
-std::string Win32GetFileDescriptionByExtension(const std::string& extension) {
-	HKEY hKey;
-	if (RegOpenKeyExA(HKEY_CLASSES_ROOT, NULL, 0, KEY_READ, &hKey) == ERROR_SUCCESS) {
-		std::string subKey = Win32GetDefaultValueFromRegistry(hKey, extension);
-		std::string description = Win32GetDefaultValueFromRegistry(hKey, subKey);
-		RegCloseKey(hKey);
-		return description;
-	}
-	return "";
-}
-HICON Win32GetFileIconByExtension(const std::string& extension) {
-	HKEY hKey;
-	if (RegOpenKeyExA(HKEY_CLASSES_ROOT, NULL, 0, KEY_READ, &hKey) == ERROR_SUCCESS) {
-		std::string subKey = Win32GetDefaultValueFromRegistry(hKey, extension);
-		std::string iconPath = Win32GetDefaultValueFromRegistry(hKey, subKey + "\\DefaultIcon");
-		RegCloseKey(hKey);
-		if (iconPath.size() == 0)
-			return NULL;
-		size_t pos = iconPath.find_last_of(',');
-		std::string path = iconPath.substr(0, pos);
-		int index = std::stoi(iconPath.substr(pos + 1));
-		char expandedPath[MAX_PATH];
-		ExpandEnvironmentStringsA(path.c_str(), expandedPath, MAX_PATH);
-		HICON smallIcon;
-		ExtractIconExW(std::wstring(expandedPath, expandedPath + strlen(expandedPath)).c_str(), index, NULL, &smallIcon, 1);
-		return smallIcon;
-	}
-	return NULL;
-}
-BITMAP Win32GetBitmapInfo(HICON hIcon) {
-	BITMAP bmp;
-	ICONINFO iconInfo;
-	GetIconInfo(hIcon, &iconInfo);
-	GetObject(iconInfo.hbmColor, sizeof(BITMAP), &bmp);
-	return bmp;
-}
-std::vector<unsigned char> Win32ConvertHICONToRGBA(HICON hIcon, size_t& ww, size_t& hh) {
-	BITMAP bmp = Win32GetBitmapInfo(hIcon);
-	int width = bmp.bmWidth;
-	int height = bmp.bmHeight;
-	ww = width;
-	hh = height;
-	HDC hdc = CreateCompatibleDC(NULL);
-	BITMAPINFO bmi;
-	ZeroMemory(&bmi, sizeof(BITMAPINFO));
-	bmi.bmiHeader.biSize = sizeof(BITMAPINFOHEADER);
-	bmi.bmiHeader.biWidth = width;
-	bmi.bmiHeader.biHeight = -height;
-	bmi.bmiHeader.biPlanes = 1;
-	bmi.bmiHeader.biBitCount = 32;
-	bmi.bmiHeader.biCompression = BI_RGB;
-	unsigned char* bits = new unsigned char[width * height * 4];
-	HBITMAP hBmp = CreateDIBSection(hdc, &bmi, DIB_RGB_COLORS, (void**)&bits, NULL, 0);
-	SelectObject(hdc, hBmp);
-	DrawIconEx(hdc, 0, 0, hIcon, width, height, 0, NULL, DI_NORMAL);
-	std::vector<unsigned char> rgba;
-	for (int i = 0; i < width * height * 4; i += 4) {
-		rgba.push_back(bits[i + 2]);
-		rgba.push_back(bits[i + 1]);
-		rgba.push_back(bits[i]);
-		rgba.push_back(bits[i + 3]);
-	}
-	DeleteObject(hBmp);
-	DeleteDC(hdc);
-	return rgba;
-}
-#endif
 
 namespace vgui
 {
-	static std::map<std::string, IconImage*> g_dicExtensionIcons;
-
 	class FileCompletionMenu : public Menu
 	{
 	public:
@@ -1265,27 +1067,6 @@ void FileOpenDialog::NewFolder(char const* folderName)
 		++i;
 	} while (i <= 999);
 }
-IImage* FileOpenDialog::GetIconFromExtension(const char* extension){
-#ifdef WIN32
-	auto it = g_dicExtensionIcons.find(extension);
-	if (it != g_dicExtensionIcons.end())
-		return it->second;
-	else {
-		HICON icon = Win32GetFileIconByExtension(extension);
-		if (icon == NULL)
-			return nullptr;
-		size_t iw, ih;
-		auto bits = Win32ConvertHICONToRGBA(icon, iw, ih);
-		DestroyIcon(icon);
-		IconImage* img = new IconImage(bits.data(), iw, ih);
-		std::string key = extension;
-		g_dicExtensionIcons.insert(std::make_pair(key, img));
-		return img;
-	}
-#else
-	return nullptr;
-#endif // WIN32
-}
 
 void FileOpenDialog::GetFileDescriptionByExtension(const char* extension, char* buf){
 #ifdef WIN32
@@ -1402,9 +1183,9 @@ void FileOpenDialog::PopulateFileList()
 				if (std::find_if(aryFilters.begin(), aryFilters.end(), [&extension](std::string& a) -> bool {return WildMatch(extension, a); }) == aryFilters.end())
 					continue;
 			}
-			IImage* img = GetIconFromExtension(extension.c_str());
-			if (img)
-				kv->SetPtr("iconImage", (void*)img);
+			IImage* image = surface()->GetIconImageForFullPath(iter.path().u8string().c_str());
+			if (image)
+				kv->SetPtr("iconImage", (void*)image);
 			kv->SetInt("image", 1);
 			kv->SetInt("imageSelected", 1);
 			kv->SetInt("directory", 0);
