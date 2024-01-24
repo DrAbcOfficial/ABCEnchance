a472fcd8db111679a0deb8c37bddfe4336479b13
hzqst <113660872@qq.com>
2024-01-24 19:22:55 +0800

    Shutdown tasks when closing / restarting game.

--- src/Source/Utility/curl.cpp
+++ src/Source/Utility/curl.cpp
@@ -1,3 +1,5 @@
+#include <metahook.h>
+#include "plugins.h"
 #include "curl.h"
 #include <wtypes.h>
 
@@ -9,12 +11,19 @@ void (*curl_easy_cleanup)(void* curl) = nullptr;
 struct curl_slist* (*curl_slist_append)(struct curl_slist*, const char*) = nullptr;
 void (*curl_slist_free_all)(struct curl_slist* list) = nullptr;
 
-HMODULE hCurl;
-void LoadLibcurl() {
-	hCurl = GetModuleHandle("libcurl.dll");
-	if(!hCurl)
-		hCurl = LoadLibrary("libcurl.dll");
-#define LIBCURL_FUNCTION_DEFINE(name) name = (decltype(name))(GetProcAddress(hCurl, #name))
+HMODULE g_hLibCurl = NULL;
+
+void LoadLibcurl()
+{
+	g_hLibCurl = LoadLibrary("libcurl.dll");
+
+	if (!g_hLibCurl)
+	{
+		SYS_ERROR("Could not load libcurl.dll");
+		return;
+	}
+
+#define LIBCURL_FUNCTION_DEFINE(name) name = (decltype(name))(GetProcAddress(g_hLibCurl, #name))
 	LIBCURL_FUNCTION_DEFINE(curl_easy_init);
 	LIBCURL_FUNCTION_DEFINE(curl_slist_append);
 	LIBCURL_FUNCTION_DEFINE(curl_easy_setopt);
@@ -24,6 +33,11 @@ void LoadLibcurl() {
 	LIBCURL_FUNCTION_DEFINE(curl_easy_cleanup);
 }
 
-void CloseLibcurl() {
-	FreeLibrary(hCurl);
+void FreeLibcurl()
+{
+	if (g_hLibCurl)
+	{
+		FreeLibrary(g_hLibCurl);
+		g_hLibCurl = NULL;
+	}
 }
