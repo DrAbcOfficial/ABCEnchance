13874087d1999506d2ac21256737eb1ccb7b59d5
Abc Dr <me@drabc.net>
2024-02-12 12:37:54 +0800

    update for new header

--- VGUI2/GameUI/GameUI.cpp
+++ VGUI2/GameUI/GameUI.cpp
@@ -6,118 +6,79 @@
 
 extern void SetAdvanceOptPanelVisible(bool state);
 
-class CVGUI2Extension_GameUICallbacks : public IVGUI2Extension_GameUICallbacks
-{
+class CVGUI2Extension_GameUICallbacks : public IVGUI2Extension_GameUICallbacks{
 public:
-	int GetAltitude() const override
-	{
+	virtual int GetAltitude() const override{
 		return 0;
 	}
-	const char* GetControlModuleName() const
-	{
+	virtual const char* GetControlModuleName() const{
 		return "ABCEnchance";
 	}
-	void Initialize(CreateInterfaceFn* factories, int count) override{
+	virtual void Initialize(CreateInterfaceFn* factories, int count) override{
 	}
-	void Start(struct cl_enginefuncs_s* engineFuncs, int interfaceVersion, void* system) override
-	{
+	virtual void Start(struct cl_enginefuncs_s* engineFuncs, int interfaceVersion, void* system) override{
 		BackGroundInitMusic();
 	}
 
-	void Shutdown(void) override
-	{
+	virtual virtual void Shutdown(void) override{
 
 	}
-
-	void ActivateGameUI(VGUI2Extension_CallbackContext* CallbackContext) override
-	{
+	virtual void ActivateGameUI(VGUI2Extension_CallbackContext* CallbackContext) override{
 
 	}
-
-	void ActivateDemoUI(VGUI2Extension_CallbackContext* CallbackContext) override
-	{
+	virtual void ActivateDemoUI(VGUI2Extension_CallbackContext* CallbackContext) override{
 
 	}
-
-	void HasExclusiveInput(VGUI2Extension_CallbackContext* CallbackContext) override
-	{
+	virtual void HasExclusiveInput(VGUI2Extension_CallbackContext* CallbackContext) override{
 
 	}
-
-	void RunFrame(VGUI2Extension_CallbackContext* CallbackContext) override
-	{
+	virtual void RunFrame(VGUI2Extension_CallbackContext* CallbackContext) override{
 
 	}
-
-	void ConnectToServer(const char*& game, int& IP, int& port, VGUI2Extension_CallbackContext* CallbackContext) override
-	{
+	void ConnectToServer(const char*& game, int& IP, int& port, VGUI2Extension_CallbackContext* CallbackContext) override{
 		BackGroundSetDecodeState(false);
 	}
-
-	void DisconnectFromServer(VGUI2Extension_CallbackContext* CallbackContext) override
-	{
+	virtual void DisconnectFromServer(VGUI2Extension_CallbackContext* CallbackContext) override{
 		BackGroundSetDecodeState(true);
 	}
-
-	void HideGameUI(VGUI2Extension_CallbackContext* CallbackContext) override
-	{
+	virtual void HideGameUI(VGUI2Extension_CallbackContext* CallbackContext) override{
 		SetAdvanceOptPanelVisible(false);
 	}
-
-	void IsGameUIActive(VGUI2Extension_CallbackContext* CallbackContext) override
-	{
+	virtual void IsGameUIActive(VGUI2Extension_CallbackContext* CallbackContext) override{
 
 	}
-
-	void LoadingStarted(const char*& resourceType, const char*& resourceName, VGUI2Extension_CallbackContext* CallbackContext) override
-	{
+	virtual void LoadingStarted(const char*& resourceType, const char*& resourceName, VGUI2Extension_CallbackContext* CallbackContext) override{
 
 	}
-
-	void LoadingFinished(const char*& resourceType, const char*& resourceName, VGUI2Extension_CallbackContext* CallbackContext) override
-	{
+	virtual void LoadingFinished(const char*& resourceType, const char*& resourceName, VGUI2Extension_CallbackContext* CallbackContext) override{
 
 	}
-
-	void StartProgressBar(const char*& progressType, int& progressSteps, VGUI2Extension_CallbackContext* CallbackContext) override
-	{
+	virtual void StartProgressBar(const char*& progressType, int& progressSteps, VGUI2Extension_CallbackContext* CallbackContext) override{
 
 	}
-
-	void ContinueProgressBar(int& progressPoint, float& progressFraction, VGUI2Extension_CallbackContext* CallbackContext) override
-	{
+	virtual void ContinueProgressBar(int& progressPoint, float& progressFraction, VGUI2Extension_CallbackContext* CallbackContext) override{
 
 	}
-
-	void StopProgressBar(bool& bError, const char*& failureReason, const char*& extendedReason, VGUI2Extension_CallbackContext* CallbackContext) override
-	{
+	virtual void StopProgressBar(bool& bError, const char*& failureReason, const char*& extendedReason, VGUI2Extension_CallbackContext* CallbackContext) override{
 
 	}
-
-	void SetProgressBarStatusText(const char*& statusText, VGUI2Extension_CallbackContext* CallbackContext) override
-	{
+	virtual void SetProgressBarStatusText(const char*& statusText, VGUI2Extension_CallbackContext* CallbackContext) override{
 
 	}
-
-	void SetSecondaryProgressBar(float& progress, VGUI2Extension_CallbackContext* CallbackContext) override
-	{
+	virtual void SetSecondaryProgressBar(float& progress, VGUI2Extension_CallbackContext* CallbackContext) override{
 
 	}
-
-	void SetSecondaryProgressBarText(const char*& statusText, VGUI2Extension_CallbackContext* CallbackContext) override
-	{
+	virtual void SetSecondaryProgressBarText(const char*& statusText, VGUI2Extension_CallbackContext* CallbackContext) override{
 
 	}
-
 };
 static CVGUI2Extension_GameUICallbacks s_GameUICallbacks;
 
-class CVGUI2Extension_GameUIKeyValuesCallbacks : public IVGUI2Extension_GameUIKeyValuesCallbacks{
+class CVGUI2Extension_KeyValuesCallbacks : public IVGUI2Extension_KeyValuesCallbacks{
 public:
-	int GetAltitude() const override{
+	virtual int GetAltitude() const override{
 		return 0;
 	}
-
 	virtual void KeyValues_LoadFromFile(void*& pthis, IFileSystem*& pFileSystem, const char*& resourceName, const char*& pathId, const char* sourceModule, VGUI2Extension_CallbackContext* CallbackContext) {
 		if (CallbackContext->IsPost && !strcmp(resourceName, "resource/GameMenu.res")) {
 			bool* pRealReturnValue = (bool*)CallbackContext->pRealReturnValue;
@@ -149,19 +110,17 @@ public:
 		}
 	}
 };
-
-static CVGUI2Extension_GameUIKeyValuesCallbacks s_GameUIKeyValuesCallbacks;
+static CVGUI2Extension_KeyValuesCallbacks s_GameUIKeyValuesCallbacks;
 
 extern void OpenAdvanceOptPanel(vgui::Panel* pthis);
 class CVGUI2Extension_TaskBarCallbacks : public IVGUI2Extension_GameUITaskBarCallbacks {
-	int GetAltitude() const override{
+	virtual int GetAltitude() const override{
 		return 0;
 	}
 	virtual void CTaskBar_ctor(IGameUITaskBarCtorCallbackContext* CallbackContext) override {
-
 	}
 	virtual void CTaskBar_OnCommand(void*& pPanel, const char*& command, VGUI2Extension_CallbackContext* CallbackContext) override {
-		if (!strcmp(command, "OpenOptionsABCEnchanceDialog")) {
+		if (!std::strcmp(command, "OpenOptionsABCEnchanceDialog")) {
 			OpenAdvanceOptPanel(reinterpret_cast<vgui::Panel*>(pPanel));
 		}
 	}
@@ -177,13 +136,13 @@ GameUI init & shutdown
 
 void GameUI_InstallHooks(void){
 	VGUI2Extension()->RegisterGameUICallbacks(&s_GameUICallbacks);
-	VGUI2Extension()->RegisterGameUIKeyValuesCallbacks(&s_GameUIKeyValuesCallbacks);
+	VGUI2Extension()->RegisterKeyValuesCallbacks(&s_GameUIKeyValuesCallbacks);
 	VGUI2Extension()->RegisterGameUITaskBarCallbacks(&s_TaskBarCallbacks);
 }
 
 void GameUI_UninstallHooks(void){
 	VGUI2Extension()->UnregisterGameUICallbacks(&s_GameUICallbacks);
-	VGUI2Extension()->UnregisterGameUIKeyValuesCallbacks(&s_GameUIKeyValuesCallbacks);
+	VGUI2Extension()->UnregisterKeyValuesCallbacks(&s_GameUIKeyValuesCallbacks);
 	VGUI2Extension()->UnregisterGameUITaskBarCallbacks(&s_TaskBarCallbacks);
 }
 
