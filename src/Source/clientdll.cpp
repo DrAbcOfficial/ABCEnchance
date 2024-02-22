42db9249f8391ad4c056bd8962fb5a39c43c5cf5
Dr.Abc <me@drabc.net>
2024-02-23 00:42:53 +0800

    yeah, thats it

--- src/Source/clientdll.cpp
+++ src/Source/clientdll.cpp
@@ -3,23 +3,24 @@
 #include "CCustomHud.h"
 
 class IClientDLL : public IBaseInterface {
-	virtual int Init(char* interface_char_ptr) = 0;
-	virtual void PlaySoundFX(float* param_1, int param_2, char* param_3, float param_4, float param_5, int param_6, int param_7) = 0;
-	virtual void RenderBegin(ref_params_s* param_1) = 0;
+	virtual int Init(char*) = 0;
+	virtual void PlaySoundFX(float*, int, char*, float, float, int, int) = 0;
+	virtual void RenderBegin(ref_params_s*) = 0;
 	virtual void RenderEnd1() = 0;
 	virtual void RenderEnd2() = 0;
 };
-static int(__fastcall* m_pfnCClientDLL_Init)(void* pthis, int, char* interface_char_ptr) = nullptr;
-static void(__fastcall* m_pfnCClientDLL_PlaySoundFX)(void* pthis, int, float* param_1, int param_2, char* param_3, float param_4, float param_5, int param_6, int param_7) = nullptr;
-static void(__fastcall* m_pfnCClientDLL_RenderBegin)(void* pthis, int, ref_params_s* param_1) = nullptr;
+static int(__fastcall* m_pfnCClientDLL_Init)(void* pthis, int, char*) = nullptr;
+static void(__fastcall* m_pfnCClientDLL_PlaySoundFX)(void* pthis, int, float*, int, char*, float, float, int, int) = nullptr;
+static void(__fastcall* m_pfnCClientDLL_RenderBegin)(void* pthis, int, ref_params_s*) = nullptr;
 static void(__fastcall* m_pfnCClientDLL_RenderEnd1)(void* pthis, int) = nullptr;
 static void(__fastcall* m_pfnCClientDLL_RenderEnd2)(void* pthis, int) = nullptr;
+
 class CNewClientDLL : public IClientDLL {
 	virtual int Init(char* interface_char_ptr) override {
 		return m_pfnCClientDLL_Init(this, 0, interface_char_ptr);
 	}
-	virtual void PlaySoundFX(float* param_1, int param_2, char* param_3, float param_4, float param_5, int param_6, int param_7) override {
-		m_pfnCClientDLL_PlaySoundFX(this, 0, param_1, param_2, param_3, param_4, param_5, param_6, param_7);
+	virtual void PlaySoundFX(float* origin, int channel, char* name, float fvol, float attenuation, int extraflags, int pitch) override {
+		m_pfnCClientDLL_PlaySoundFX(this, 0, origin, channel, name, fvol, attenuation, extraflags, pitch);
 	}
 	virtual void RenderBegin(ref_params_s* param_1) {
 		gCustomHud.RenderRadar();
