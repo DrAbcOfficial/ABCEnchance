5fae14345f23ba839eb217a25799df693b37b5ad
Abc Dr <me@drabc.net>
2024-02-24 19:35:56 +0800

    用继承

--- src/Header/HUD/vgui/ammostack.h
+++ src/Header/HUD/vgui/ammostack.h
@@ -1,62 +1,27 @@
 #ifndef CAMMOSTACKPANEL_H
 #define CAMMOSTACKPANEL_H
-#include <vector>
-#include <vgui_controls/EditablePanel.h>
-#include "IViewportPanel.h"
+#include "basestackpanel.h"
 
-namespace vgui {
-	class ImageSprPanel;
-}
-
-class CAmmoStackItem : public vgui::EditablePanel {
+class CAmmoStackItem : public CBaseSprStackItem {
 public:
-	DECLARE_CLASS_SIMPLE(CAmmoStackItem, vgui::EditablePanel);
+	DECLARE_CLASS_SIMPLE(CAmmoStackItem, CBaseSprStackItem);
 	CAmmoStackItem(Panel* parent, int spridx, int value, int l, int r, int t, int b, float expire, float fi, float fo, float is);
-
 	virtual void ApplySchemeSettings(vgui::IScheme* pScheme) override;
 	virtual void PerformLayout() override;
 	int GetValue();
-	void SetImage(const char* image);
-	void Show(float flTime);
 
-	void SetExpire(float f);
-	void CheckExpire();
 private:
 	vgui::Label* m_pText;
-	vgui::ImageSprPanel* m_pPanel;
-
-	int iSprIdx;
 	int iValue;
-	float fKeepTime;
-	float fIn;
-	float fOut;
 };
 
-class CAmmoStackPanel : public vgui::EditablePanel, public IViewportPanel
-{
+class CAmmoStackPanel : public CBaseStackPanel{
 public:
-	DECLARE_CLASS_SIMPLE(CAmmoStackPanel, vgui::EditablePanel);
-
+	DECLARE_CLASS_SIMPLE(CAmmoStackPanel, CBaseStackPanel);
 	CAmmoStackPanel();
 	virtual void ApplySchemeSettings(vgui::IScheme* pScheme) override;
 	// IViewportPanel overrides
 	virtual const char* GetName() override;
-	virtual void Reset() override;
-	virtual void ApplySettings(KeyValues* inResourceData) override;
-	virtual void OnThink() override;
-	virtual void PaintBackground() override;
-	virtual void ShowPanel(bool state) override;
-	virtual bool IsVisible() override;
-	virtual vgui::VPANEL GetVPanel() override;
-	virtual void SetParent(vgui::VPANEL parent) override;
-
 	void AddAmmoPickup(int id, int count);
-
-private:
-	float m_flFadeinTime;
-	float m_flFadeoutTime;
-	float m_flKeepTime;
-	float m_flIconSize;
-	std::vector<CAmmoStackItem*> m_aryAmmos;
 };
 #endif
---
+++ src/Header/HUD/vgui/basestackpanel.h
@@ -0,0 +1,50 @@
+#ifndef CBASESTACKPANEL_H
+#define CBASESTACKPANEL_H
+#include <vector>
+#include <vgui_controls/EditablePanel.h>
+#include "IViewportPanel.h"
+
+namespace vgui {
+	class ImageSprPanel;
+}
+
+class CBaseSprStackItem : public vgui::EditablePanel {
+public:
+	DECLARE_CLASS_SIMPLE(CBaseSprStackItem, vgui::EditablePanel);
+	CBaseSprStackItem(vgui::Panel* parent, int spridx, int l, int r, int t, int b, float expire, float fi, float fo, float is);
+	void Show(float flTime);
+
+	void SetExpire(float f);
+	void CheckExpire();
+protected:
+	vgui::ImageSprPanel* m_pPanel;
+
+	int iSprIdx;
+	float fKeepTime;
+	float fIn;
+	float fOut;
+};
+
+class CBaseStackPanel : public vgui::EditablePanel, public IViewportPanel{
+public:
+	DECLARE_CLASS_SIMPLE(CBaseStackPanel, vgui::EditablePanel);
+	CBaseStackPanel(vgui::Panel* parent, const char* name);
+	virtual const char* GetName() = 0;
+	virtual void Reset();
+	virtual void ShowPanel(bool state);
+	virtual bool IsVisible();
+	virtual vgui::VPANEL GetVPanel();
+	virtual void SetParent(vgui::VPANEL parent);
+
+	virtual void ApplySettings(KeyValues* inResourceData) override;
+	virtual void OnThink() override;
+	virtual void PaintBackground() override;
+
+protected:
+	float m_flFadeinTime;
+	float m_flFadeoutTime;
+	float m_flKeepTime;
+	float m_flIconSize;
+	std::vector<CBaseSprStackItem*> m_aryPanels;
+};
+#endif
