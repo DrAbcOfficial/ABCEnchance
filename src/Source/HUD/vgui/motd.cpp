ff6fd4bb1b922693ef03074ed39509feda1cf6ff
Dr.Abc <me@drabc.net>
2024-03-03 00:33:19 +0800

    use cache file again, yeah

--- src/Source/HUD/vgui/motd.cpp
+++ src/Source/HUD/vgui/motd.cpp
@@ -19,7 +19,7 @@
 #define VIEWPORT_MOTD_NAME "MotdPanel"
 extern vgui::HScheme GetViewPortBaseScheme();
 CMotdPanel::CMotdPanel()
-	: BaseClass(nullptr, VIEWPORT_MOTD_NAME){
+	: BaseClass(nullptr, VIEWPORT_MOTD_NAME) {
 	SetProportional(true);
 	SetKeyBoardInputEnabled(false);
 	SetMouseInputEnabled(false);
@@ -31,26 +31,27 @@ CMotdPanel::CMotdPanel()
 	m_pHTML = new vgui::HTML(this, "HTMLMessage", false, false);
 
 	gCVars.pMotd = CREATE_CVAR("hud_motd", "1", FCVAR_VALUE, [](cvar_t* cvar) {
-		switch (static_cast<int>(cvar->value)){
-			case 1:
-			case -1 : g_pViewPort->GetMotdPanel()->ShowPanel(false); break;
-			default: break;
+		switch (static_cast<int>(cvar->value)) {
+		case 1:
+		case -1: g_pViewPort->GetMotdPanel()->ShowPanel(false); break;
+		default: break;
 		}
-	});
+		});
 
 	LoadControlSettings(VGUI2_ROOT_DIR "MotdPanel.res");
 	SetVisible(false);
 }
-CMotdPanel::~CMotdPanel(){
+CMotdPanel::~CMotdPanel() {
 	RemoveTempHTML();
 }
-const char* CMotdPanel::GetName(){
+const char* CMotdPanel::GetName() {
 	return VIEWPORT_MOTD_NAME;
 }
-void CMotdPanel::Reset(){
+void CMotdPanel::Reset() {
 	if (IsVisible())
 		ShowPanel(false);
 	RemoveTempHTML();
+	//m_pHTML->OpenURL("", nullptr);
 	m_aryClipedPage.clear();
 	m_szBuffer.clear();
 	m_bLoadedMissionBrief = false;
@@ -58,13 +59,13 @@ void CMotdPanel::Reset(){
 	m_bBuiledPages = false;
 	m_iNowPage = 0;
 }
-void CMotdPanel::ApplySchemeSettings(vgui::IScheme* pScheme){
+void CMotdPanel::ApplySchemeSettings(vgui::IScheme* pScheme) {
 	BaseClass::ApplySchemeSettings(pScheme);
 	SetBgColor(GetSchemeColor("Motd.BgColor", GetSchemeColor("Panel.BgColor", pScheme), pScheme));
 	m_pMessage->SetBgColor(GetSchemeColor("Motd.MessageBgColor", GetSchemeColor("Panel.BgColor", pScheme), pScheme));
 	m_pProgressBar->SetBgColor(GetSchemeColor("Motd.ProgressBgColor", GetSchemeColor("Panel.BgColor", pScheme), pScheme));
 }
-void CMotdPanel::ApplySettings(KeyValues* inResourceData){
+void CMotdPanel::ApplySettings(KeyValues* inResourceData) {
 	BaseClass::ApplySettings(inResourceData);
 	const char* labelText = inResourceData->GetString("keep_time", NULL);
 	if (labelText)
@@ -73,23 +74,23 @@ void CMotdPanel::ApplySettings(KeyValues* inResourceData){
 	if (labelText)
 		m_flFadeTime = std::stof(labelText);
 }
-void CMotdPanel::ShowPanel(bool state){
+void CMotdPanel::ShowPanel(bool state) {
 	if (state == IsVisible())
 		return;
 	if (state)
 		SetAlpha(255);
 	SetVisible(state);
 }
-bool CMotdPanel::IsVisible(){
+bool CMotdPanel::IsVisible() {
 	return BaseClass::IsVisible();
 }
-vgui::VPANEL CMotdPanel::GetVPanel(){
+vgui::VPANEL CMotdPanel::GetVPanel() {
 	return BaseClass::GetVPanel();
 }
-void CMotdPanel::SetParent(vgui::VPANEL parent){
+void CMotdPanel::SetParent(vgui::VPANEL parent) {
 	BaseClass::SetParent(parent);
 }
-void CMotdPanel::OnThink(){
+void CMotdPanel::OnThink() {
 	float flTime = m_flShowTime - gEngfuncs.GetClientTime();
 	if (flTime > 0)
 		m_pProgressBar->SetWide(GetWide() * (flTime / m_flKeepTime));
@@ -131,15 +132,16 @@ void CMotdPanel::BuildPage() {
 void CMotdPanel::AddBuffer(std::string szBuffer) {
 	m_szBuffer += szBuffer;
 }
-void CMotdPanel::AppendMotd(char* message){
+void CMotdPanel::AppendMotd(char* message) {
 	AddBuffer(message);
 }
-void CMotdPanel::ChangePage(size_t iNewPage){;
+void CMotdPanel::ChangePage(size_t iNewPage) {
+	;
 	m_flShowTime = gEngfuncs.GetClientTime() + m_flKeepTime;
 	m_iNowPage = iNewPage;
-	m_pMessage->SetText(m_aryClipedPage[m_iNowPage].c_str());	
+	m_pMessage->SetText(m_aryClipedPage[m_iNowPage].c_str());
 }
-void CMotdPanel::ShowMotd(){
+void CMotdPanel::ShowMotd() {
 	if (gCVars.pMotd->value < 1)
 		return;
 	if (m_bInHTML) {
@@ -152,13 +154,19 @@ void CMotdPanel::ShowMotd(){
 	if (m_aryClipedPage.size() > 0) {
 		ChangePage(0);
 		ShowPanel(true);
-	}	
+	}
 }
-void CMotdPanel::FinishSendMOTD(){
+void CMotdPanel::FinishSendMOTD() {
 	//check html
 	if (m_szBuffer.starts_with("<!DOCTYPE html>")) {
-		m_szBuffer = "data:text/html," + m_szBuffer;
-		m_pHTML->OpenURL(m_szBuffer.c_str(), nullptr, true);
+		FileHandle_t file = vgui::filesystem()->Open("abcenchance/motd_cache.html", "w+");
+		if (file)
+			vgui::filesystem()->Write(m_szBuffer.c_str(), m_szBuffer.size(), file);
+		vgui::filesystem()->Close(file);
+		char local[MAX_PATH] = "file:///";
+		const size_t uiURLLength = 8;
+		vgui::filesystem()->GetLocalPath("abcenchance/motd_cache.html", local + uiURLLength, 252);
+		m_pHTML->OpenURL(local, nullptr, true);
 		m_pMessage->SetVisible(false);
 		m_pHTML->SetVisible(true);
 		m_bInHTML = true;
@@ -177,9 +185,11 @@ void CMotdPanel::FinishSendMOTD(){
 	}
 	m_szBuffer.clear();
 }
-void CMotdPanel::RemoveTempHTML(){
+void CMotdPanel::RemoveTempHTML() {
+	if (vgui::filesystem()->FileExists("abcenchance/motd_cache.html"))
+		vgui::filesystem()->RemoveFile("abcenchance/motd_cache.html");
 }
-void CMotdPanel::LoadMissionBrief(){
+void CMotdPanel::LoadMissionBrief() {
 	if (!m_bLoadedMissionBrief) {
 		m_bLoadedMissionBrief = true;
 		char buf[1024];
@@ -190,7 +200,7 @@ void CMotdPanel::LoadMissionBrief(){
 		FileHandle_t file = vgui::filesystem()->Open(mappath.c_str(), "r");
 		char* p = buf;
 		std::string buffer;
-		while (p && *p){
+		while (p && *p) {
 			p = vgui::filesystem()->ReadLine(buf, 1023, file);
 			buffer += buf;
 		}
