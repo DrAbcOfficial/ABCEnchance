344bd8bb7da3449ce5f00809474bff83845086f2
Abc Dr <me@drabc.net>
2025-11-23 00:00:45 +0800

    update to latest metahook

--- src/core/metahook/MetaRenderer.cpp
+++ src/core/metahook/MetaRenderer.cpp
@@ -12,26 +12,22 @@ IMetaRenderer* MetaRenderer()
 
 void MetaRenderer_Init()
 {
-	g_hMetaRenderer = (HINTERFACEMODULE)GetModuleHandleA("Renderer.dll");
-
-	if (g_hMetaRenderer)
+	mh_plugininfo_t rendererInfo{};
+	if (!g_pMetaHookAPI->GetPluginInfoByBaseFileName("Renderer", &rendererInfo)) {
+		SYS_ERROR("Could not get Renderer");
+		return;
+	}
+	g_hMetaRenderer = (HINTERFACEMODULE)rendererInfo.PluginModuleBase;
+	auto factory = Sys_GetFactory(g_hMetaRenderer);
+	if (!factory)
 	{
-		auto factory = Sys_GetFactory(g_hMetaRenderer);
-
-		if (!factory)
-		{
-			SYS_ERROR("Could not get factory from Renderer.dll");
-			return;
-		}
-
-		g_pMetaRenderer = (decltype(g_pMetaRenderer))factory(METARENDERER_INTERFACE_VERSION, NULL);
-
-		if (!g_pMetaRenderer)
-		{
-			SYS_ERROR("Could not get g_pMetaRenderer from Renderer.dll");
-			return;
-		}
-
+		SYS_ERROR("Could not get factory from Renderer");
+		return;
+	}
+	g_pMetaRenderer = (decltype(g_pMetaRenderer))factory(METARENDERER_INTERFACE_VERSION, NULL);
+	if (!g_pMetaRenderer)
+	{
+		SYS_ERROR("Could not get interface from Renderer");
 		return;
 	}
 }
